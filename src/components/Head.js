a10d5c86fbcfd3eb13b64604b44524e00638ca12
SHUBHAM <swardshub@gmail.com>
2024-08-18 01:03:26 +0530

    searchBar

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,13 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { CiSearch } from "react-icons/ci";
 import { useDispatch } from "react-redux";
 import { toggleMenu } from "../utils/appSlice";
+import { YOUTUBE_SEARCH_API } from "../utils/constant";
 
 const Head = () => {
   const dispatch = useDispatch();
 
   const toggleMenuHandler = () => {
     dispatch(toggleMenu());
+  };
+
+  const [searchQuery, setSearchQuery] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+  const [showSuggestions, setShowSuggestions] = useState(false);
+
+  useEffect(() => {
+    // console.log(searchQuery);
+    // call the api after 2 milisecond
+    const timer = setTimeout(() => {
+      getSearchSuggestion();
+    }, 200);
+
+    // if we call the api again within 2 millisecond then first unmount the existing api and then again set the timer of 2 millisecond
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [searchQuery]);
+
+  const getSearchSuggestion = async () => {
+    const data = await fetch(YOUTUBE_SEARCH_API + searchQuery);
+    const json = await data.json();
+    setSuggestions(json[1]);
+    // console.log(json);
   };
 
   return (
@@ -28,14 +54,37 @@ const Head = () => {
 
       {/* search */}
       <div className="col-span-10">
-        <input
-          placeholder="Type here"
-          className="border border-gray-300 rounded-l-full h-8 w-1/2 p-2"
-          type="search"
-        />
-        <button className=" border border-black-300 px-5 py-2 h-8 rounded-r-full bg-gray-100">
-          <CiSearch />
-        </button>
+        <div>
+          <input
+            placeholder="Type here"
+            onFocus={() => setShowSuggestions(true)}
+            onBlur={() => setShowSuggestions(false)}
+            value={searchQuery}
+            onChange={(e) => {
+              setSearchQuery(e.target.value);
+            }}
+            className="border border-gray-300 rounded-l-full h-8 w-1/2 p-2"
+            type="search"
+          />
+          <button className=" border border-black-300 px-5 py-2 h-8 rounded-r-full bg-gray-100">
+            <CiSearch />
+          </button>
+        </div>
+        {showSuggestions && (
+          <div className="fixed  bg-white py-2 px-2 w-[37rem] shadow-lg rounded-lg border border-gray-100">
+            <ul>
+              {suggestions.map((s) => (
+                <li
+                  key={s}
+                  className="flex gap-2 py-2 px-3 shadow-sm hover:bg-gray-100"
+                >
+                  <CiSearch />
+                  {s}
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
       </div>
 
       {/* user Icon */}
